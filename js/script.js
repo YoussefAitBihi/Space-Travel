3bdbbb79031835c97280f8c77fccd176cf224578
Youssef Ait bihi <youssefaitbihi@gmail.com>
2023-04-25 20:34:26 +0100

    Fix the underline issue

--- js/script.js
+++ js/script.js
@@ -1,35 +1,56 @@
 'use strict';
 
+// /**
+//  * Tab component
+//  */
+// const tabBox = document.querySelector('[data-tab="tab-links-parent"]');
+
+// /**
+//  * Handle the links using event delegation technique
+//  */
+// tabBox.addEventListener('click', function (e) {
+//   // 1. Check if the clicked element is the link or span inside the link
+//   if (
+//     e.target.classList.contains('tab-link') ||
+//     e.target.parentElement.classList.contains('tab-link')
+//   ) {
+//     const tabs = this.querySelectorAll('.tab-link');
+//     const target = e.target;
+
+//     // 2. Remove the active class
+//     tabs.forEach((tab) => {
+//       tab
+//         .querySelector('.tab-link__underline')
+//         .classList.remove('tab-link__underline--scaled');
+//     });
+
+//     // 3. Add the active class
+//     target
+//       .querySelector('.tab-link__underline')
+//       .classList.add('tab-link__underline--scaled');
+//   }
+// });
+
 /**
  * Tab component
  */
-const allTabBox = document.querySelectorAll('[data-tab="tab-links-parent"]');
+const tabBox = document.querySelector('[data-tab="parent"]');
 
-/**
- * Handle the links using event delegation technique
- */
+tabBox.addEventListener('click', function (e) {
+  // 1. If the clicked element is not that we want
+  if (e.target.dataset?.tab !== 'child') return;
 
-allTabBox.forEach((tabBox) => {
-  tabBox.addEventListener('click', function (e) {
-    // 1. Check if the clicked element is the link or span inside the link
-    if (
-      e.target.classList.contains('tab-link') ||
-      e.target.parentElement.classList.contains('tab-link')
-    ) {
-      const tabs = this.querySelectorAll('.tab-link');
-      const target = e.target;
+  const target =
+    e.target.closest('li') || e.target.closest('button') || e.target;
 
-      // 2. Remove the active class
-      tabs.forEach((tab) => {
-        tab
-          .querySelector('.tab-link__underline')
-          .classList.remove('tab-link__underline--scaled');
-      });
+  // 2. Unselect all tabs
+  const tabs =
+    tabBox.querySelectorAll('li') || tabBox.querySelectorAll('button');
 
-      // 3. Add the active class
-      target
-        .querySelector('.tab-link__underline')
-        .classList.add('tab-link__underline--scaled');
-    }
+  tabs.forEach((tab) => {
+    tab.classList.remove('underline');
   });
+
+  // 3. Select the target element (clicked element)
+  target.classList.add('underline');
 });
